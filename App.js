35fe7fe994db3f9bb6c5e466148ddfe1d23aaed8
Dat Nguyen <datns1610@gmail.com>
2021-05-05 16:03:17 +0700

    change 1.1

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
-    <View>
-
-    </View>
+    <>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-
-});
---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { StatusBar, StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { Searchbar } from "react-native-paper";
+
+export const RestaurantsScreen = () => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar />
+    </View>
+    <View style={styles.list}>
+      <Text>list</Text>
+    </View>
+  </SafeAreaView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+  search: {
+    padding: 16,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: "blue",
+    padding: 16,
+  },
+});
